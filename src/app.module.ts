aa9ef5379fd52c266735359d08288c58f5e5e520
Camilo Colmenares <j.colmenares@uniandes.edu.co>
2023-11-30 08:16:12 -0500

    config modules main inicial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: "postgres",
+      password: "camilol47",
+      database: "postgres",
+      entities: [],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+  ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
